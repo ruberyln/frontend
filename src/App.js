3393b760bcab390dee4cce999bdbca630e24fe07
Jesutofunmi <tofunalabi@gmail.com>
2022-05-30 20:15:38 -0600

    new commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Navbar from "./components/navbar";
+//import {SwipeableViews} from "./components/swipeableviews";
+//import {BrowserRouter as Router} from "react-router-dom"
+//import { Routes, Route, Link } from "react-router-dom";
+import SignUp from './components/signup';
+//import SignIn from './components/signin';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+//import Typography from '@mui/material/Typography';
+
+function App () {
+    return (
+        <div>
+        <Navbar/>
+       
+        </div>
+    )
 }
-
+  
 export default App;
